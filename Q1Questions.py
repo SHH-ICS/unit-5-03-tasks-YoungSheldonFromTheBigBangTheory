c0f948145968812d4c77a03022cc82630ed67fc2
YoungSheldonFromTheBigBangTheory <124380188+YoungSheldonFromTheBigBangTheory@users.noreply.github.com>
2023-05-16 14:47:56 +0000

    g

--- Q1Questions.py
+++ Q1Questions.py
@@ -1,2 +1,22 @@
 # Create a program that will accept a multiple choice question, four answers, and the letter of correct answer. 
 # This will be six lines, and then store the questions in the file questions.txt.
+
+#q1 = input(": ")
+import json
+
+Q = input("Input Question: ")
+
+questions = []
+for i in range(4):
+    questions.append(input(f"Answer {i + 1}: "))
+
+answer = str(input("Input Correct Answer: "))
+
+Json = {"Question":Q, "Answers":questions, "Correct": answer}
+
+with open("Question.json", "w") as f:
+    f.write(json.dumps(Json))
+
+#filehandle = open("questions.txt", 'w')
+
+#filehandle.write(input("Input Question: "))
--- Q2Quiz.py
+++ Q2Quiz.py
@@ -1,2 +1,6 @@
 # Create a second program that will read the file questions.txt, formatted as described above, and pose the questions to the user. 
 # The program will keep score of the number of questions answered correctly.
+import json
+
+with open("Question.json", "w") as f:
+    f.read(json.dumps(Json))
